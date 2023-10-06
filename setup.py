d627676c76c8efe841713a17bb422849f8f7f144
Habbestad <ehabbestad@outlook.com>
2023-10-06 21:46:57 +0200

    refactoring

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from Cython.Build import cythonize
 
 sourcefiles = ["py_wrappers.pyx"]
 
-extensions = Extension("py_wrappers",  sourcefiles, language="c++", extra_compile_args = ["-std=c++20"])
+extensions = Extension("py_wrappers",  sourcefiles, language = "c++", extra_compile_args = ["-std=c++20"])
 
 setup(ext_modules=cythonize(extensions,compiler_directives={'language_level' : "3"}))
