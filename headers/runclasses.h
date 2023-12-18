bbbc9840b570b77dd384cd0852c9f44170555c14
Habbestad <ehabbestad@outlook.com>
2023-12-18 18:20:12 +0100

    added gitignore

---
+++ headers/runclasses.h
@@ -0,0 +1,131 @@
+#ifndef RUNCLASSES_H
+#define RUNCLASSES_H
+
+#include "readers.h"
+#include "timestamp.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <vector>
+#include <algorithm>
+
+class RunSummary
+{
+    /* A RunSummary holds an ID, time and distance. It can be initialized from a .tcx file or manually.
+       It is also the return type of the Run method getRunSummary(), see below */
+
+private:
+    std::string m_id;
+    float m_runTime; //{0.0}; //in seconds
+    float m_runDist; //{0.0}; //in km
+
+public:
+    RunSummary();
+    RunSummary(std::ifstream& infile);    // Initialize with filestream object,
+    RunSummary(const std::string& str);   // or with path string,
+    RunSummary(float totalTime, float totalDist, const std::string& lapID); // or manually.
+
+    std::string getId()  const;
+    float getRunTime() const;
+    float getRunDist() const;
+    float getAverageSpeed() const;
+
+    void print() const;
+
+    friend class Run;
+};
+
+class Track
+{
+    /* This class is just an intuitive container for storage of trackpoints (usually collected from a .tcx file). */
+
+private:
+    int m_time {0};
+    float m_distance {0.0};
+    float m_altitude {0.0};
+    float m_speed {0.0};
+    float m_heartRate {0.0};
+
+public:
+    Track();
+    Track(int time, float distance, float altitude, float speed, float heartRate = 0.0);
+
+    friend class Run;
+    friend class Workout;
+};
+
+using TrackVecIt = std::vector<Track>::iterator;
+
+class Run 
+{
+    /* This is the main class of the project, and is most commonly initialized from reading a .tcx file.
+       It holds and ID, "raw tracks", methods for manually extracting sections of the run, and some "getters" */
+
+protected:
+    std::string m_id;
+    std::vector<Track> m_tracks;
+
+    /* Some convenience functions to find start and end of sections: */
+    TrackVecIt findClosestTrackByTime(int measure);         
+    TrackVecIt findClosestTrackByDistance(int measure);
+
+    /* Extract a section by providing iterators to m_tracks */
+    Run getSection(const TrackVecIt& first, 
+                   const TrackVecIt& last, 
+                   const std::string& sectionName,
+                   bool cumulative = true);
+
+public:
+    Run();                          
+    Run(std::ifstream& infile);     // Initialize with filestream object
+    Run(const std::string& str);    // or with path string
+    Run(const std::vector<Track>& tracks, const std::string& id); // or custom
+
+    /* Manually extract a section from your run, by specifying start and end points by time or distance.
+       Tracks are not evenly distributed, so the functions takess the closest values. As a consequence, a
+       a section can be slightly longer or shorter than wanted. */
+    Run getSectionByTime( int startTime, int endTime, const std::string& sectionName, bool cumulative = false);
+    Run getSectionByDistance(int start, int end, const std::string& sectionName, bool cumulative = false);
+
+    std::string getId() const;
+    RunSummary getRunSummary();
+
+    std::vector<int> getTimes();
+    std::vector<float> getDistances();
+    std::vector<float> getSpeeds();
+    std::vector<float> getAltitudes();
+    std::vector<float> getHR();
+    int getTotalTime();
+    float getTotalDistance();
+};
+
+
+class Workout : public Run
+{
+    /* A Workout is a Run which also holds the laps defined in the .tcx file. Any .tcx file contains at least one lap (so 
+       strictly speaking any Run can be seen as a Workout). The laps show up in the file according to the settings on the watch
+       and/or if the file corresponds to a predefined workout. */
+
+private:
+    std::vector<TrackVecIt> m_lapIts;
+    std::vector<int> m_startTimes;
+
+public:
+    Workout(std::ifstream& infile);
+    Workout(const std::string& dir);
+
+    Run getLap(int lapNumber);
+
+    std::vector<int> getStartTimes();
+
+};
+
+
+#endif
+
+
+// bool isStop(const TrackVecIt& it, int threshold = 30);
+/* Maybe make function that finds big gaps between tracks.
+    Use this to not get stupid results for average speed? */ 
---
+++ headers/timestamp.h
@@ -0,0 +1,48 @@
+#ifndef TIMESTAMP_H
+#define TIMESTAMP_H
+
+#include "runclasses.h"
+#include "readers.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <vector>
+
+// A TimeStamp holds the time and date of a run. Made this to easily choose and sort files by time and date.
+class TimeStamp
+{
+private:
+    std::string m_date;
+    int m_hour;
+    int m_min;
+    int m_sec;
+
+public:
+    TimeStamp();
+
+    TimeStamp(const std::string& time_stamp);
+
+    TimeStamp(const std::string& date, const std::string& time);
+
+    TimeStamp(const std::string& date, int zero);
+
+    int getYear() const;
+    int getMonth() const;
+    int getDay()  const;
+    std::string getDate();
+
+    int secondsPast(int initial_time = 0) const;
+
+    int secondsPast(const TimeStamp& time_stamp) const;
+
+    // checks if the *this TimeStamp is earlier than stamp.
+    bool isBefore(const TimeStamp& stamp) const;
+
+};
+
+
+
+
+#endif
